f27f58d21596c8592008050dadb5b3efcec3eb07
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-06-20 10:58:31 +0300

    Unordered list with Hero names was displayed in a template of the app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,11 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
       <h1>{{title}}</h1>
+      <h2>My Heroes</h2>
+      <ul class="heroes">
+        <li>
+        </li>
+      </ul>   
       <h2>{{hero.name}} details!</h2>
       <div><label>id:</label>{{hero.id}}</div>
       <div>
