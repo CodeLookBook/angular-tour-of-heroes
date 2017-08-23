39313f3c2264554179c38f7ad5da8ed4e8ec9471
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-08-23 11:06:52 +0300

    'gotoDetail()' method was added to 'heroes.component.ts'

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -11,7 +12,10 @@ import { HeroService } from './hero.service';
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
-  constructor(private heroService: HeroService) { }
+  constructor(
+      private heroService: HeroService,
+      private router: Router,
+      ) { }
   ngOnInit(): void {
     this.getHeroes();
   }
@@ -20,5 +24,8 @@ export class HeroesComponent implements OnInit {
   }
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes );
+  }
+  gotoDetail(){
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
