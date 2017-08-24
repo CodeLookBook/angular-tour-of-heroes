621ad6dd9d8749f363263fc4fd6cee88c1884fe7
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-08-24 15:29:42 +0300

    'hero.service.ts' was updated.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
+import { Headers, Http } from '@angular/http';
+
 import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+    private heroesUrl = 'api/heroes';  // URL to web api
+    constructor(private http: Http) { }
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
     getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-            .then(heroes => heroes.find(hero => hero.id === id));
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
